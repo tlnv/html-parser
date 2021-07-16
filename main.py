40753b185d11caeef2c51025190361963063ab72
TNA110 <73946838+TNA110@users.noreply.github.com>
2021-07-16 21:42:55 +0300

    Добавил комменты

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import parse_all_cources
 import parse_cash
 import get_all_urls
 from datetime import datetime
-import threading
 
 
-starttime = datetime.now()
 exchange_adresses, cash_adresses = get_all_urls.get_all_exchange_adresses()
-#parse_all_cources.parse_all_courses(cash_adresses)
+# parse_all_cources.parse_all_courses(cash_adresses) - запуск парсинга всех направлений
+# parse_cash.parse_all_cash(cash_adresses) - запуск парсинга кэша по городам
+
 parse_cash.parse_all_cash(cash_adresses)
